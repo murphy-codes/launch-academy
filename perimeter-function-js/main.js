401a2522eb78cd7839dd08064e1b6e3f57f502d4
Tom Murphy <56618913+murphy-codes@users.noreply.github.com>
2020-07-03 20:54:41 -0700

    Week 2: Javascript Working With Data Structures → Perimeter Function JS

---
+++ perimeter-function-js/main.js
@@ -0,0 +1,12 @@
+/* 
+Launch Academy
+Assignment: Perimeter Function JS
+Author: Tom Murphy
+Last Edited: 2020-06-23 17:27 PT
+*/
+
+let perimeter = (width, height) => {
+  return (width + height) * 2;
+}
+
+console.log(`A rectangle with a width of 3 and a height of 5 has a perimeter of ${perimeter(3, 5)}.`);
