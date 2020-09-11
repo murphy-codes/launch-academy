6c097ff13f03924f472fa878509a73dbe0732373
Tom Murphy <56618913+murphy-codes@users.noreply.github.com>
2020-09-10 22:08:59 -0700

    Week 13: Hibernate → Java Movie Validations

--- Week_13/java-movie-validations/src/main/java/Main.java
+++ Week_13/java-movie-validations/src/main/java/Main.java
@@ -12,7 +12,6 @@ public class Main {
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.launchacademy.movie_database");
     EntityManager em = emf.createEntityManager();
 
-//    em.getTransaction().begin();
     Movie movie = new Movie();
     ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
     Validator validator = factory.getValidator();
@@ -21,66 +20,8 @@ public class Main {
       System.out.println(violation.getPropertyPath() + ": " + violation.getMessage());
     }
 
-
-
+//    em.getTransaction().begin();
 //    em.persist(movie);
 //    em.getTransaction().commit();
-
-
-
-//    try {
-//      // our database interactions can go here
-//
-//      // Find by ID
-////      Movie movie = em.find(Movie.class, 2L);
-////      System.out.println(movie);
-////      System.out.println(movie.getId());
-////      System.out.println(movie.getTitle());
-//
-//      // Find by title
-////      TypedQuery<Movie> query = em.createQuery("SELECT m FROM Movie m WHERE title = 'Troll 2'", Movie.class);
-//      // Multiple results expected
-////      List<Movie> movies = query.getResultList();
-////      if(movies.size() > 0) {
-////        System.out.println(movies.get(0).getTitle());
-////      }
-////      else {
-////        System.out.println("NOT FOUND");
-////      }
-//      // Single result expected
-////      Movie standalone = query.getSingleResult();
-////      System.out.println(standalone.getTitle());
-//      // Persisting an Entity
-////      Movie movie = new Movie();
-////      movie.setTitle("Tequila Mockingbird");
-////      movie.setGenreId(2L);
-////      movie.setYear(2025);
-////      em.getTransaction().begin();
-////      em.persist(movie);
-////      em.getTransaction().commit();
-////      System.out.println(movie.getId());
-//      // Checking my work ...
-////      Movie movie = em.find(Movie.class, 3549L);
-////      System.out.println(movie);
-////      System.out.println(movie.getId());
-////      System.out.println(movie.getTitle());
-////      System.out.println(movie.getRating());
-//      // Updating the Entity
-////      TypedQuery<Movie> retrieval = em.createQuery("SELECT m FROM Movie m WHERE title = 'Tequila Mockingbird'", Movie.class);
-////      Movie bestEver = retrieval.getResultList().get(0);
-////      em.getTransaction().begin();
-////      bestEver.setRating(100);
-////      em.getTransaction().commit();
-//      // Updating via JPQL
-////      em.getTransaction().begin();
-////      Query updateQuery = em.createQuery("UPDATE Movie SET rating = :newRating WHERE title = 'Tequila Mockingbird'");
-////      updateQuery.setParameter("newRating", 400);
-////      int updateCount = updateQuery.executeUpdate();
-////      em.getTransaction().commit();
-//    }
-//    finally {
-//      em.close();
-//      emf.close();
-//    }
   }
 }
