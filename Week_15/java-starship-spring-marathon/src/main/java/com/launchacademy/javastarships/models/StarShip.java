4cb084699fefc72c17b30ee66f6eb5a47a6ea770
Tom Murphy <56618913+murphy-codes@users.noreply.github.com>
2020-09-23 15:36:29 -0700

    Week 15: Spring Controllers And Views → The Final Frontier Needs Ships!

---
+++ Week_15/java-starship-spring-marathon/src/main/java/com/launchacademy/javastarships/models/StarShip.java
@@ -0,0 +1,43 @@
+package com.launchacademy.javastarships.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StarShip {
+  private Integer id;
+  private String name;
+  private String description;
+  private Integer fuelCapacity;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Integer getFuelCapacity() {
+    return fuelCapacity;
+  }
+
+  public void setFuelCapacity(Integer fuelCapacity) {
+    this.fuelCapacity = fuelCapacity;
+  }
+}
---
+++ Week_15/java-starship-spring-marathon/src/main/java/com/launchacademy/javastarships/services/StarShipService.java
@@ -0,0 +1,10 @@
+package com.launchacademy.javastarships.services;
+
+import com.launchacademy.javastarships.models.StarShip;
+import java.util.List;
+
+public interface StarShipService {
+  List<StarShip> findAll();
+  void addToList(StarShip starShip);
+  StarShip get(Integer index);
+}
