95f32f773a4b7eeb77d86008060a3ae819913b80
Tom Murphy <56618913+murphy-codes@users.noreply.github.com>
2020-09-25 01:17:52 -0700

    Week 15: Spring Controllers And Views → Java Team Roster with Spring

--- Week_15/java-team-roster-with-spring/src/main/java/com/launchacademy/teamrosterwithspring/controllers/PositionsController.java
+++ Week_15/java-team-roster-with-spring/src/main/java/com/launchacademy/teamrosterwithspring/controllers/PositionsController.java
@@ -1,20 +1,42 @@
 package com.launchacademy.teamrosterwithspring.controllers;
+
+import com.launchacademy.teamrosterwithspring.models.League;
+import com.launchacademy.teamrosterwithspring.models.Player;
+import com.launchacademy.teamrosterwithspring.models.Team;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.view.RedirectView;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/positions")
 public class PositionsController {
   @GetMapping
   public String getIndex(Model model) {
+    List<Team> teams = League.getLeague().getTeams();
+    List<String> positions = new ArrayList<>();
+    for (Team team: teams) { for (Player player: team.getPlayers()) { if (!positions.contains(player.getPosition())) { positions.add(player.getPosition()); } } }
+    model.addAttribute("positions", positions);
     return "positions/index";
   }
 
-  @GetMapping("/{name}")
-  public String getShow(Model model) {
+  @GetMapping("/{positionName}")
+  public String getShow(@PathVariable String positionName, Model model) {
+    List<Team> teams = League.getLeague().getTeams();
+    List<String> players = new ArrayList<>();
+    List<String> teamNames = new ArrayList<>();
+    for (Team team: teams) { for (Player player: team.getPlayers()) {
+      if (player.getPosition().equals(positionName)) {
+        players.add(player.getName());
+        teamNames.add(team.getTeamName());
+      }
+    } }
+    model.addAttribute("position", positionName);
+    model.addAttribute("players", players);
+    model.addAttribute("teamNames", teamNames);
     return "positions/show";
   }
 }
