b593553a9289498cb40d046f037a1cfb3a1cffe5
Tom Murphy <56618913+murphy-codes@users.noreply.github.com>
2020-10-05 09:43:39 -0700

    Week 16: Api Development With Spring → Java Team Roster with Spring Part Duex

--- Week_16/java-team-roster-with-spring-pt-2/src/main/java/com/launchacademy/teamrosterwithspring/repositories/PlayerRepository.java
+++ Week_16/java-team-roster-with-spring-pt-2/src/main/java/com/launchacademy/teamrosterwithspring/repositories/PlayerRepository.java
@@ -1,7 +1,10 @@
 package com.launchacademy.teamrosterwithspring.repositories;
 
 import com.launchacademy.teamrosterwithspring.models.Player;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface PlayerRepository extends CrudRepository<Player, Integer> {
