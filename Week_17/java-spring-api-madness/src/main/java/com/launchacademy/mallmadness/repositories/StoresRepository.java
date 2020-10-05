4ac16a2f9bcb6eabc12f1aee2a948bd9fc45a209
Tom Murphy <56618913+murphy-codes@users.noreply.github.com>
2020-10-05 11:57:16 -0700

    Week 17: Spring React Monoliths → Java Spring Api Madness

---
+++ Week_17/java-spring-api-madness/src/main/java/com/launchacademy/mallmadness/repositories/StoresRepository.java
@@ -0,0 +1,11 @@
+package com.launchacademy.mallmadness.repositories;
+
+import com.launchacademy.mallmadness.models.Store;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StoresRepository extends PagingAndSortingRepository<Store, Integer> {
+  public boolean existsByName(String storeName);
+  public Store findFirstById(Integer storeId);
+}
